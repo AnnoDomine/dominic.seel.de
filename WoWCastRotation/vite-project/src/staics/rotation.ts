8d7f2ee1af38f8b56dfe00b6395b7623486c0453
dominic.seel <dominic.seel@futurenext.de>
2025-04-27 07:12:15 +0200

    feat: initialize WoWCastRotation project with Vite and React
    - Add main application structure with App component and CSS styles.
    - Implement casting logic with defined casts and their properties.
    - Create static files for SVG logos and styles.
    - Set up TypeScript configuration for the project.
    - Configure Vite for React with SWC plugin.

---
+++ WoWCastRotation/vite-project/src/staics/rotation.ts
@@ -0,0 +1,12 @@
+import { Casts } from "./casts";
+
+type Rotation = Array<Casts>;
+
+export const rotation: Rotation = [
+    "mirror_image",
+    "evocation",
+    "arcane_missiles",
+    "arcane_surge",
+    "arcan_barrage",
+    "touch_of_the_magi",
+];
