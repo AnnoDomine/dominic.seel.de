42080bc8e5e6025ca01cf511708a8019ef4e8100
dominic.seel <dominic.seel@futurenext.de>
2025-12-14 11:27:35 +0100

    fix: Fix double definition

--- apps/backend/core/settings.py
+++ apps/backend/core/settings.py
@@ -24,8 +24,8 @@ ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").sp
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = BASE_DIR / os.environ.get("DJANGO_STATIC_ROOT", "staticfiles")
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Application definition
 
@@ -164,12 +164,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-STATIC_ROOT = BASE_DIR / os.environ.get("DJANGO_STATIC_ROOT", "staticfiles")
-STATIC_URL = "static/"
 
 
 # Default primary key field type
