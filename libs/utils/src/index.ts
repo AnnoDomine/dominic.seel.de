255480cc89ae32d584e74b1bc99f1a5c3a6acc91
dominic.seel <dominic.seel@futurenext.de>
2025-12-14 11:25:06 +0100

    fix(frontend): Remove unused export to fix CI error

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -2,4 +2,3 @@ export * from "./lib/utils";
 export * from "./utils/dataGridHelpers/dataGridHelpers";
 export * from "./utils/openInNewTab/openInNewTab";
 export * from "./utils/technologyIcons/technologyIcons";
-export * from "./utils/useInfinityScroll/useInfinityScroll";
