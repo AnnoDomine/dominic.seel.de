613b0103d9abe4e304e7455acb10c18357ccc49d
Dominic Seel <AnnoDomine@users.noreply.github.com>
2026-01-01 20:32:45 +0100

    Fix/fix deployment pipeline (#28)
    * fix(ci): reverse last changes
    
    * fix(ci): reverse last changes
    
    * fix(ci): reverse last changes
    
    ---------
    
    Co-authored-by: dominic.seel <dominic.seel@futurenext.de>

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -36,6 +36,7 @@ export default defineConfig(() => ({
         commonjsOptions: {
             transformMixedEsModules: true,
         },
+        target: "esnext",
     },
     test: {
         watch: false,
